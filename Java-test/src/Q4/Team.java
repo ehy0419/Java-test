60b558584a465178f18f54fc582c91fdd50d8c0f
ehy0419 <2hy0419@naver.com>
2025-07-30 18:01:15 +0900

    q5. 포기..

--- Java-test/src/Q4/Team.java
+++ Java-test/src/Q4/Team.java
@@ -20,7 +20,7 @@ class Team {
     }
 
     // 기능
-    public void addPlayer(Player player /* TODO: 올바른 매개변수를 선언하세요 */) {
+    public void addPlayer(Player player    /* TODO: 올바른 매개변수를 선언하세요 */) {
         // TODO: 매개변수로 받은 player 객체를 playerList에 추가하세요.
         playerList.add(player);
     }
@@ -37,3 +37,13 @@ class Team {
         }
     }
 }
+
+/// 제네릭 : 클래스나 메서드가 다룰 데이터 타입을 외부에서 결정할 수 있게 해주는 문법
+//ArrayList<String> strList;        // 문자열만 저장하는 리스트
+//ArrayList<Integer> intList;       // 정수만 저장하는 리스트
+//ArrayList<Player> playerList;     // Player 객체만 저장하는 리스트
+
+//ArrayList<T> 어떤 타입이 들어올지 모르니 <T> 라는 제네릭 타입으로,
+//ArrayList<Player> 여기에서는 Player 만 넣을 수 있다고 제한한 것이다. 'Player를 저장하는 상자 타입을 미리 지정한 것'
+
+///  제네릭, 스트림, 람다 에 대해서 공부하자..
